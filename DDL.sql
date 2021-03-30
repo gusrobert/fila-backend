862a43a6f1bdfe986875c41e36cc2b580af3ee59
GUSTAVO ROBERT MOURA DA CONCEICAO <guromoura4321@gmail.com>
2021-03-29 21:05:32 -0300

    Update project

--- DDL.sql
+++ DDL.sql
@@ -2,25 +2,76 @@ create schema fila;
 
 use fila;
 
-drop user user@localhost;
-flush privileges;
-
 create user user@localhost identified by 'pass123';
 
 grant select, insert, delete, update on fila.* to user@localhost;
+
+CREATE TABLE perfil (
+    id bigint unsigned not null auto_increment,
+    nome varchar(255),
+    primary key (id)
+);
+
+CREATE TABLE credencial (
+    id bigint unsigned not null auto_increment,
+    login varchar(255) UNIQUE,
+    senha varchar(255),
+    sn_bloqueado varchar(255),
+    sn_excluido varchar(255),
+    sn_online varchar(255),
+    perfil bigint unsigned NOT NULL,
+	primary key (id),
+	FOREIGN KEY (perfil) REFERENCES perfil(id)
+);
+
+CREATE TABLE credencial_perfil (
+    id_credencial bigint unsigned not null auto_increment,
+    id_perfil bigint unsigned NOT NULL,
+    primary key (id_credencial, id_perfil),
+	foreign key (id_credencial) references credencial(id) on delete restrict on update cascade,
+	foreign key (id_perfil) references perfil(id) on delete restrict on update cascade
+);
+ 
+
+CREATE TABLE usuario (
+    id bigint unsigned not null auto_increment,
+    email varchar(255) not null unique,
+    senha varchar(255) not null,
+	credencial bigint unsigned,
+    primary key (id),
+	foreign key (credencial) references credencial(id)
+);
+
+CREATE TABLE aluno (
+    id bigint unsigned not null auto_increment,
+    nome varchar(45),
+    usuario bigint unsigned,
+    primary key (id),
+	foreign key (usuario) references usuario(id)
+);
 
 CREATE TABLE grupo (
     id bigint unsigned not null auto_increment,
     nome varchar(45),
     aluno bigint unsigned,
-    primary key (id)
+    primary key (id),
+	foreign key (aluno) references aluno(id)
+);
+
+CREATE TABLE professor (
+    id bigint unsigned not null auto_increment,
+    nome varchar(45),
+    usuario bigint unsigned,
+    primary key (id),
+	foreign key (usuario) references usuario(id)
 );
 
 CREATE TABLE fila (
     id bigint unsigned not null auto_increment,
     professor bigint unsigned,
     hora_inicio TIMESTAMP,
-    primary key (id)
+    primary key (id),
+	foreign key (professor) references professor(id)
 );
 
 CREATE TABLE apresentacao (
@@ -28,53 +79,7 @@ CREATE TABLE apresentacao (
     grupo bigint unsigned,
     fila bigint unsigned,
     nome varchar(45),
-    primary key (id)
+    primary key (id),
+	foreign key (fila) references fila(id),
+	foreign key (grupo) references grupo(id)
 );
-
-CREATE TABLE aluno (
-    id bigint unsigned not null auto_increment,
-    nome varchar(45),
-    usuario bigint unsigned,
-    primary key (id)
-);
-
-CREATE TABLE usuario (
-    id bigint unsigned not null auto_increment,
-    email varchar(45) not null,
-    senha varchar(16) not null,
-    primary key (id)
-);
-
-CREATE TABLE professor (
-    id bigint unsigned not null auto_increment,
-    nome varchar(45),
-    usuario bigint unsigned,
-    primary key (id)
-);
- 
-ALTER TABLE grupo ADD CONSTRAINT FK_grupo_3
-    FOREIGN KEY (aluno)
-    REFERENCES aluno (id);
- 
-ALTER TABLE fila ADD CONSTRAINT FK_fila_2
-    FOREIGN KEY (professor)
-    REFERENCES professor (id);
- 
-ALTER TABLE apresentacao ADD CONSTRAINT FK_apresentacao_2
-    FOREIGN KEY (fila)
-    REFERENCES fila (id)
-    ON DELETE RESTRICT;
- 
-ALTER TABLE apresentacao ADD CONSTRAINT FK_apresentacao_3
-    FOREIGN KEY (grupo)
-    REFERENCES grupo (id);
- 
-ALTER TABLE aluno ADD CONSTRAINT FK_aluno_2
-    FOREIGN KEY (usuario)
-    REFERENCES usuario (id)
-    ON DELETE CASCADE;
- 
-ALTER TABLE professor ADD CONSTRAINT FK_professor_2
-    FOREIGN KEY (usuario)
-    REFERENCES usuario (id)
-	ON DELETE CASCADE;
---
+++ DROPTABLES.sql
@@ -0,0 +1,2 @@
+DROP database IF EXISTS fila;
+drop user IF EXISTS user@localhost;
